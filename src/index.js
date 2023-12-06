d3ff740474ac851eb19c3ceb705fa2cf558de575
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2023-12-06 17:27:05 -0300

    fix: remove api unused code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 
 import { Database } from "./local-database.js";
 
 const server = Fastify();
+await server.register(cors, { "Access-Control-Allow-Origin": "*" });
+
 const database = new Database();
 
-server.post("/summarization", (request, response) => {
+server.post("/summarizations", (request, response) => {
   const { title, link, startAt, endAt } = request.body;
 
   database.create({ title: title, link: link, startAt: startAt, endAt: endAt });
 
-  return response.status(201).send();
+  return response.status(201);
 });
 
-server.get("/summarization", (request, response) => {
+server.get("/summarizations", (request, response) => {
   const search = request.query.search;
   const summarizations = database.list(search);
 
   return response.status(200).send(summarizations);
 });
 
-server.put("/summarization/:id", (request, response) => {
+server.put("/summarizations/:id", (request, response) => {
   const summarizationId = request.params.id;
   const { title, link, startAt, endAt } = request.body;
 
@@ -31,14 +34,14 @@ server.put("/summarization/:id", (request, response) => {
     endAt: endAt,
   });
 
-  return response.status(204).send();
+  return response.status(204)
 });
 
-server.delete("/summarization/:id", (request, response) => {
+server.delete("/summarizations/:id", (request, response) => {
   const summarizationId = request.params.id;
   database.delete(summarizationId);
 
-  return response.status(200).send();
+  return response.status(200)
 });
 
 server.listen({ port: 3030 });
