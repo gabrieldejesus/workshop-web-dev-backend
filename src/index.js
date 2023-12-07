617d0ea1fd2e2afef0bd2211088d573b6b35eeb4
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2023-12-07 18:46:48 -0300

    feat: finish route to create a new summarization

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import { z } from "zod";
 import ytdl from "ytdl-core";
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { unlink } from "node:fs";
 import ffmpeg from "fluent-ffmpeg";
 import { randomUUID } from "node:crypto";
+import { AssemblyAI } from "assemblyai";
 
 import { Database } from "./local-database.js";
 
 const server = Fastify();
+const assemblyAiClient = new AssemblyAI({
+  apiKey: process.env.ASSEMBLYAI_API_KEY,
+});
+
 await server.register(cors, { "Access-Control-Allow-Origin": "*" });
 
 const database = new Database();
@@ -24,6 +30,15 @@ const audioSchema = z.object({
 });
 
 server.post("/summarizations", async (request, response) => {
+  /**
+   * ✅ Receber os dados do meu front-end
+   * ✅ Validar os dados recebidos do meu back-end
+   * ✅ Transformar o video do link em um áudio e salvar ele temporariamente na pasta public/audios
+   * ✅ Enviar o audio para a API do assemblyai e pegar o resumo e transcrição
+   * ✅ Salvar as informações do resumo no banco de dados local
+   * ✅ Excluir o audio depois de criar a transcrição
+   */
+
   try {
     const { title, link, startAt, endAt } = request.body;
 
@@ -32,7 +47,7 @@ server.post("/summarizations", async (request, response) => {
     const videoReadableStream = ytdl(data.link, { quality: "lowestaudio" });
 
     const filename = `${randomUUID()}.mp3`;
-    const outputFolder = "public/audios/"
+    const outputFolder = "public/audios/";
 
     const ffmpegCommand = ffmpeg(videoReadableStream)
       .noVideo()
@@ -49,19 +64,27 @@ server.post("/summarizations", async (request, response) => {
       ffmpegCommand.run();
     });
 
-    /**
-     * ✅ Receber os dados do meu front-end
-     * ✅ Validar os dados recebidos do meu front-end
-     * ✅ Transformar o video do link em um áudio e salvar ele temporariamente na pasta public/audios
-     * Enviar o audio para a API do assemblyai e pegar o job id daquela tarefa
-     * Salvar o id do job no banco de dados
-     */
+    const params = {
+      audio: `${outputFolder}${filename}`,
+      summarization: true,
+      summary_model: "informative",
+      summary_type: "bullets",
+    };
+
+    const transcript = await assemblyAiClient.transcripts.transcribe(params);
 
     database.create({
       title: title,
       link: link,
       startAt: startAt,
       endAt: endAt,
+      transcript: transcript.text,
+      summary: transcript.summary,
+    });
+
+    unlink(`${outputFolder}${filename}`, (err) => {
+      if (err) return console.log(err);
+      console.log(`${outputFolder}${filename} deleted successfully`);
     });
 
     return response.status(201).send({ success: true });
