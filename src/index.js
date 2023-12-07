d339d0de44f26eb16683ad74d55711e45b10041a
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2023-12-07 16:50:11 -0300

    chore: work in progress validate data and transform video in a audio file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import { z } from "zod";
+import ytdl from "ytdl-core";
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import ffmpeg from "fluent-ffmpeg";
+import { randomUUID } from "node:crypto";
 
 import { Database } from "./local-database.js";
 
@@ -8,20 +12,62 @@ await server.register(cors, { "Access-Control-Allow-Origin": "*" });
 
 const database = new Database();
 
-server.post("/summarizations", (request, response) => {
-  const { title, link, startAt, endAt } = request.body;
+const audioSchema = z.object({
+  title: z.string(),
+  link: z
+    .string()
+    .regex(
+      new RegExp(/https:\/\/www\.youtube\.com\/watch\?v=[a-zA-Z0-9_-]{11}/)
+    ),
+  startAt: z.number(),
+  endAt: z.number(),
+});
 
-  /**
-   * Receber os dados do meu front-end
-   * Validar os dados recebidos do meu front-end
-   * Transformar o video do link em um áudio e salvar ele temporariamente na pasta public/audios
-   * Enviar o audio para a API do assemblyai e pegar o job id daquela tarefa
-   * Salvar o id do job no banco de dados
-   */
+server.post("/summarizations", async (request, response) => {
+  try {
+    const { title, link, startAt, endAt } = request.body;
 
-  database.create({ title: title, link: link, startAt: startAt, endAt: endAt });
+    const data = audioSchema.parse({ title, link, startAt, endAt });
 
-  return response.status(201);
+    const videoReadableStream = ytdl(data.link, { quality: "lowestaudio" });
+
+    const filename = `${randomUUID()}.mp3`;
+    const outputFolder = "public/audios/"
+
+    const ffmpegCommand = ffmpeg(videoReadableStream)
+      .noVideo()
+      .audioCodec("libmp3lame")
+      .audioBitrate(128)
+      .seekInput(data.startAt)
+      .duration(data.endAt - data.startAt)
+      .format("mp3")
+      .output(`${outputFolder}${filename}`);
+
+    await new Promise((resolve, reject) => {
+      ffmpegCommand.on("end", resolve);
+      ffmpegCommand.on("error", reject);
+      ffmpegCommand.run();
+    });
+
+    /**
+     * ✅ Receber os dados do meu front-end
+     * ✅ Validar os dados recebidos do meu front-end
+     * ✅ Transformar o video do link em um áudio e salvar ele temporariamente na pasta public/audios
+     * Enviar o audio para a API do assemblyai e pegar o job id daquela tarefa
+     * Salvar o id do job no banco de dados
+     */
+
+    database.create({
+      title: title,
+      link: link,
+      startAt: startAt,
+      endAt: endAt,
+    });
+
+    return response.status(201).send({ success: true });
+  } catch (error) {
+    return response.status(500).send({ success: false, message: error });
+  }
 });
 
 server.get("/summarizations", (request, response) => {
@@ -42,14 +88,14 @@ server.put("/summarizations/:id", (request, response) => {
     endAt: endAt,
   });
 
-  return response.status(204)
+  return response.status(204);
 });
 
 server.delete("/summarizations/:id", (request, response) => {
   const summarizationId = request.params.id;
   database.delete(summarizationId);
 
-  return response.status(200)
+  return response.status(200);
 });
 
 server.listen({ port: 3030 });
